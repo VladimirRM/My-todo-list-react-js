264b2d42a2a37b504c84e999fbb8698ce766b5d0
Vladimir RM <shvdrummer@gmail.com>
2023-01-16 20:52:59 +0200

    add useState and todo,todos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [todo,setTodo]= useState('')
+  const [todos,setTodos]= useState('')
   return (
     <div className="App">
    
