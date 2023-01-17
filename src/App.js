64cfe61826c29f8f431b2f2c2142517dfed21133
Vladimir RM <shvdrummer@gmail.com>
2023-01-17 09:51:47 +0200

    create func delete todo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
-import React,{useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [todo,setTodo]= useState('')
-  const [todos,setTodos]= useState('')
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    if (todo !== "") setTodos([...todos, todo]);
+    setTodo("");
+  };
+
+  const deleteTodo = (text) => {
+       const newTodo = todos.filter(todo=>{
+        return todo !==text
+       } )
+       setTodos(newTodo)
+
+    return todo;
+  };
   return (
     <div className="App">
+      <div>
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
 
-      <input type="text" />
-      <button></button>
-   
+      {todos?.length > 0 ? (
+        <ul>
+          {todos.map((todo, id) => (
+            <div>
+              <li key={todo}>{todo}</li>
+              <button onClick={() => deleteTodo(todo.id)}>Delete todo</button>
+            </div>
+          ))}
+        </ul>
+      ) : (
+        <div>
+          <p>No task found</p>
+        </div>
+      )}
     </div>
   );
 }
