528f33fda29c5a75f992c74034652e6d55b78784
Vladimir RM <shvdrummer@gmail.com>
2023-01-18 16:28:09 +0200

    add input

--- src/App.js
+++ src/App.js
@@ -4,33 +4,15 @@ import "./index.css";
 const App = () => {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
-
-  const addTodo = (e) => {
-    if (todo !== "") {
-      setTodos([...todos, todo]);
-      setTodos("");
-    }
+     return(
+      <div>
+        <input type="text"
+        placeholder="Enter something" />
+      </div>
+     )
+ 
   };
-  return (
-    <div>
-      <input
-        type="text"
-        value={todo}
-        placeholder="Enter something"
-        onChange={(e) => setTodo(e.target.value)}
-      />
-      <button onClick={addTodo}>Add todo</button>
-      {todos?.length > 0 ? (
-        <ul>{todos.map((todo, (id) => <li key={todo.id}>{todo}</li>))}
-        
-        </ul>
-      ) : (
-        <div>
-          <p>Task not found</p>
-        </div>
-      )}
-    </div>
-  );
-};
+
+
 
 export default App;
