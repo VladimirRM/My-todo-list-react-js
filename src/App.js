00b94095a851afb9d78505564d8282a9026fa034
Vladimir RM <shvdrummer@gmail.com>
2023-01-19 20:14:13 +0200

    create input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-
-  const [todolist,setTodolist] =useState([])
+  const [todo, setTodo] = useState("");
+  const [todolist, setTodolist] = useState([]);
   return (
     <div>
+      <input
+        type="text"
+        onChange={(e) => setTodolist(e.target.value)}
+        value={todo}
+      />
       
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
