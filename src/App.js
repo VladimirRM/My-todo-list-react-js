513cc93d91743aac27f49ba0c8254f16351b946f
Vladimir RM <shvdrummer@gmail.com>
2023-01-19 20:09:40 +0200

    change code version

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
-import React, { useState } from "react";
-import "./index.css";
+import React,{useState} from 'react'
 
 const App = () => {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
 
-  const addTodo = () => {
-    setTodos([...todos, todo]);
-    
-  };
-  const handleRemove = (id) => {
-    const newTodo = todos.filter(todo, (id) => {
-      return todo.id !== id;
-    });
-
-    setTodos(newTodo);
-  };
-  const handleEdit = (id) => {
-    setTodos(todos.map((todo) => todo.id !== id));
-  };
-
+  const [todolist,setTodolist] =useState([])
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Enter something"
-        onChange={(e) => setTodo(e.target.value)}
-        value={todo}
-      />
-      <button onClick={addTodo}>Add todo</button>
-      {todos.map((todo, id) => (
-        <ul>
-          <div>
-            <li key={todo.id}>
-              {todo}
-              <button onClick={() => handleEdit(todo.id)}>Edit</button>
-              <button onClick={() => handleRemove(todo.id)}>Delete</button>
-            </li>
-          </div>
-        </ul>
-      ))}
+      
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
