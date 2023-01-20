d632b24326ade2d56821cdded9e9002960df030b
Vladimir RM <shvdrummer@gmail.com>
2023-01-20 15:37:00 +0200

    create input in form

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import React,{useState} from 'react'
 const App = () => {
 
     const [todos,setTodos ]= useState([])
+    const [userInput,setUserInput]=useState('')
 
 
     const addTodo =()=>{
 
     }
     const handleSubmit =()=>{
-
+         
     }
   return (
     <div className='App'>
         <form onSubmit={handleSubmit}>
-            
+           <input type='text'
+           onChange={handleChange}
+           value={userInput}
+           placeholder="Enter task" />  
         </form>
     </div>
   )
