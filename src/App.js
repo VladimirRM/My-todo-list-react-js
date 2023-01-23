629b8aa33e8acf846cdc8977970758a554cf61c1
Vladimir RM <shvdrummer@gmail.com>
2023-01-23 09:29:48 +0200

    change markup edit todo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React, { useState } from "react";
 
 const App = () => {
   const [input, setInput] = useState("");
-  const [todos, setTodos] = useState([{text:''}]);
+  const [todos, setTodos] = useState([{ text: "" }]);
   const [edit, setEdit] = useState(null);
-  const [value, setValue] = useState('');
-
+  const [value, setValue] = useState("");
 
   const addTodo = () => {
     const newTodo = {
@@ -22,22 +21,22 @@ const App = () => {
   const removeTodo = (id) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
-  
+
   // const editTodo = (id) => {
-    //   const text = prompt("enter text:");
-    //   setTodos(
-      //     todos.map((obj, id) => {
-        //       if (todos.id === id) {
-          //         obj.text = text;
-          //       }
-          //       return obj;
-          //     })
-          //   );
-          // };
-          
-          // const editTodo = (id) => {
-         
-          // };
+  //   const text = prompt("enter text:");
+  //   setTodos(
+  //     todos.map((obj, id) => {
+  //       if (todos.id === id) {
+  //         obj.text = text;
+  //       }
+  //       return obj;
+  //     })
+  //   );
+  // };
+
+  const editTodo = (id) => {
+
+  };
   return (
     <div>
       <input
@@ -49,14 +48,14 @@ const App = () => {
       <ul>
         {todos.map((input, id) => (
           <li key={input.id}>
-            {input.title}
-            {edit === id ? 
-            <div>
-              <input type="text"
-              value={value} />
-              <button>Save</button>
-            </div>:<div>
-              </div>}
+            {edit === id ? (
+              <div>
+                <input type="text" value={value} />
+                <button>Save</button>
+              </div>
+            ) : (
+              <div> {input.title}</div>
+            )}
             <button onClick={() => removeTodo(input.id)}>Delete</button>
             <button onClick={() => editTodo(input.id)}>Edit</button>
           </li>
