b022424b3019780ecc9ba2551d62eaa26c1d2e27
Vladimir RM <shvdrummer@gmail.com>
2023-01-23 09:26:40 +0200

    add button save

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 const App = () => {
   const [input, setInput] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([{text:''}]);
   const [edit, setEdit] = useState(null);
   const [value, setValue] = useState('');
 
@@ -22,22 +22,22 @@ const App = () => {
   const removeTodo = (id) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
-  const editTodo = (id) => {
- 
-  };
-
+  
   // const editTodo = (id) => {
-  //   const text = prompt("enter text:");
-  //   setTodos(
-  //     todos.map((obj, id) => {
-  //       if (todos.id === id) {
-  //         obj.text = text;
-  //       }
-  //       return obj;
-  //     })
-  //   );
-  // };
-
+    //   const text = prompt("enter text:");
+    //   setTodos(
+      //     todos.map((obj, id) => {
+        //       if (todos.id === id) {
+          //         obj.text = text;
+          //       }
+          //       return obj;
+          //     })
+          //   );
+          // };
+          
+          // const editTodo = (id) => {
+         
+          // };
   return (
     <div>
       <input
@@ -50,10 +50,11 @@ const App = () => {
         {todos.map((input, id) => (
           <li key={input.id}>
             {input.title}
-            {edit == id ? 
+            {edit === id ? 
             <div>
               <input type="text"
               value={value} />
+              <button>Save</button>
             </div>:<div>
               </div>}
             <button onClick={() => removeTodo(input.id)}>Delete</button>
