c4820b6757ce62be195923877d1b51602fc74675
Vladimir RM <shvdrummer@gmail.com>
2023-01-24 13:41:51 +0200

    create input and render part

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-
 const App = () => {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  const [input,setInput]= useState('')
-  const [todos,setTodos]= useState([])
-
-  const addTodo =()=>{
-     const newTodo = {
-      id:uuidv4(),
-      title:input
-     }
-     setTodos([...todos,newTodo])
-     setTodos('')
-  }
+  const addTodo = () => {
+    const newTodo = {
+      id: uuidv4(),
+      title: input,
+    };
+    setTodos([...todos, newTodo]);
+    setInput("");
+  };
   return (
     <div>
-      <input type="text"
-      onChange={(e)=>setInput(e.target.value)} 
-      value={input}/>
+      <input
+        type="text"
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+      />
       <button onClick={addTodo}>Add Task</button>
-    <ul>
-      {todos.map(input=>(
-       <li key={input.title}></li> 
-      ))}
+      <ul>
+        {todos.map(input=> (
+          <li key={uuidv4()} >{input.title}</li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
