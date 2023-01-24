8ae126e19df556b654a42d962bc41b9285338a75
Vladimir RM <shvdrummer@gmail.com>
2023-01-24 21:40:28 +0200

    create render from input

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
 
-   const [input,setInput]=useState('')
-   const [todos,setTodos]=useState([])
-
-   const addTodo =()=>{
-        const newTodo = {
-          id:Math.random(),
-          title:input
-        }
-        setTodos(newTodo)
-        setInput('')
-   }
+  const addTodo = () => {
+    const newTodo = {
+      id: Math.random(),
+      title: input,
+    };
+    setTodos([...todos,newTodo]);
+    setInput("");
+  };
   return (
     <div>
-      <input type="text"
-      onChange={(e)=>setTodos(e.target.value)} 
-      value={input}/>
+      <input
+        type="text"
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+      />
+      <button onClick={addTodo}>Add</button>
       <ul>
-
-
-        
-       {todos.map(input=>(
-        <li>{todos}</li>
-
+        {todos.map((input) => (
+          <li>{input.title}</li>
         ))}
-        </ul>
-
-
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
