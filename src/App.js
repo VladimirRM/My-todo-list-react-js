80d8959d6fdd6071a3a7a4870391843a10f66751
Vladimir RM <shvdrummer@gmail.com>
2023-01-24 10:21:23 +0200

    add const todos and empty arr

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React,{useState} from 'react'
 const App = () => {
 
   const [input,setInput]= useState('')
+  const [todos,setTodos]= useState([])
+  const [input,setInput]= useState('')
+  const [input,setInput]= useState('')
   return (
     <div>App</div>
   )
