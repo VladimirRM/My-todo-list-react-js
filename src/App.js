d74e00910ca22aebc284760a56574ac8c2c6d2bb
Vladimir RM <shvdrummer@gmail.com>
2023-01-24 12:07:17 +0200

    update code & check

--- src/App.js
+++ src/App.js
@@ -1,53 +1,74 @@
-import React,{useState} from 'react'
-import { v4 as uuidv4 } from 'uuid';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [edit, setEdit] = useState(null);
+  const [value, setValue] = useState("");
 
-  const [input,setInput]= useState('')
-  const [todos,setTodos]= useState([])
-  const [edit,setEdit]= useState(null)
-  const [value,setValue]= useState('')
-
-const addTodo=()=>{
+  const addTodo = () => {
     const newTodo = {
-      id:uuidv4(),
-      title:input,
-    }
-    setTodos([...todos,newTodo])
-
-}
-const deleteTodo =(id)=>{
-     setTodos(todos.filter(input=>input.id !==id))
-}
-const editTodo =(id)=>{
-     setTodos(todos.filter(input=>input.id !==id))
-}
-const saveTodo =(id)=>{
-     setTodos(todos.filter(input=>input.id !==id))
-}
-
+      id: uuidv4(),
+      title: input,
+    };
+    setTodos([...todos, newTodo]);
+    setInput("");
+  };
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((input) => input.id !== id));
+  };
+  const editTodo = (id, title) => {
+    setEdit(id);
+    setValue(title);
+  };
+  const saveTodo = (id) => {
+    const newTodo = [...todos].map((input) => {
+      if (input.id === id) {
+        input.title = value;
+      }
+      return input;
+    });
+    setTodos(newTodo);
+    setEdit(null);
+  };
 
   return (
     <div>
-      <input type="text"
-      onChange={(e)=>setInput(e.target.value)}
-      value={input} />
+      <input
+        type="text"
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+      />
       <button onClick={addTodo}>Add task</button>
-
-        {todos.map(input,index=>(
-             <ul>
-              <li key={index}>{input.title}</li>
-             </ul>
+      <ul>
+        {todos.map(input, (id) => (
+          <li>
+            {edit.id === id ? (
+              <div>
+                <input
+                  type="text"
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                />
+              </div>
+            ) : (
+              <div>{input.title}</div>
+            )}
+            {edit.title ? (
+              <div>
+                <button onClick={() => deleteTodo(input.id)}>Delete</button>
+                <button onClick={() => editTodo(input)}>Edit</button>
+                <button onClick={() => saveTodo(input)}>Save</button>
+              </div>
+            ) : (
+              <div></div>
+            )}
+          </li>
         ))}
-
-      <button onClick={()=>deleteTodo(input.id)}>Delete</button>
-      <button onClick={()=>editTodo(input,id)}>Edit</button>
-      <button onClick={()=>saveTodo(input,id)}>Save</button>
-
+      </ul>
     </div>
+  );
+};
 
-
-  )
-}
-
-export default App
+export default App;
