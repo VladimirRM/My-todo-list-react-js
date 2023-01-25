08abf4c67e06eca166f0e382e7489ed29c80fc83
Vladimir RM <shvdrummer@gmail.com>
2023-01-25 09:24:25 +0200

    create all func & markUp

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React,{useState} from 'react'
 const App = () => {
    const [input,setInput]=useState('')
    const [todos,setTodos]=useState([])
+   const [edit,setEdit]=useState(null)
+   const [value,setValue]=useState('')
 
    const  addTodo =()=>{
       const newTodo ={
@@ -12,14 +14,22 @@ const App = () => {
       setTodos([...todos,newTodo])
       setInput('')
    }
-   const deleteTodo =()=>{
-
+   const deleteTodo =(id)=>{
+       setTodos(todos.filter(input=>input.id !==id))
    }
-   const editTodo =()=>{
-
+   const editTodo =(id,title)=>{
+        setEdit(id)
+        setValue(title)
    }
-   const saveTodo =()=>{
-
+   const saveTodo =(id)=>{
+     const newTodo = [...todos].map(input=>{
+      if(input.id===id){
+        input.title = value
+      }
+      return input
+     })
+     setTodos(newTodo)
+     setEdit(null)
    }
 
   return (
@@ -31,7 +41,25 @@ const App = () => {
       <ul>
       {todos.map(input=>(
         <li key={input.id}>
+          {edit===input.id?
+        <div>
+          <input type="text"
+          onChange={(e)=>setValue(e.target.value)}
+          value={value} />
+        </div>  :
+        <div>
           {input.title}
+
+        </div> 
+        }
+          {input ?
+        <div>
+          <button onClick={()=>deleteTodo(input.id)}>Delete</button>
+          <button onClick={()=>editTodo(input.id,input.title)}>Edit</button>
+          <button onClick={()=>saveTodo(input.id)}>Save</button>
+        </div>  :
+        <div></div> 
+        }
         </li>
       ))}
 
