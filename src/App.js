04fd296be43e9d910e6aab67c7280c8bd0d15481
Vladimir RM <shvdrummer@gmail.com>
2023-01-25 08:48:13 +0200

    crete main state todos input

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React,{useState} from 'react'
+
+const App = () => {
+const [input,setInput]= useState('')
+const [todos,setTodos]= useState([])
+
+
+  return (
+    <div>
+    
+
+
+    </div>
+  )
+}
+
+export default App
