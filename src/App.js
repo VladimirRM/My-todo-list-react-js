43d64beceed7b47a78f53df29fbca4b931538b4b
Vladimir RM <shvdrummer@gmail.com>
2023-01-25 09:09:30 +0200

    create input & button add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const App = () => {
+   const [input,setInput]=useState('')
+   const [todos,setTodos]=useState([])
+
   return (
     <div>
+      <input type="text"
+      onChange={(e)=>setInput(e.target.value)}
+      value={input} />
+      <button onClick={addTodo}>Add</button>
       
     </div>
   )
