30afea31fe8c35982fae5684509a7a4e077fde3e
Vladimir RM <shvdrummer@gmail.com>
2023-01-25 15:55:04 +0200

    create all func and elem

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [todos, setTodos] = useState("");
-  const [input, setInput] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+  const [edit, setEdit] = useState(null);
+  const [value, setValue] = useState("");
 
-
-  const addTodo=()=>{
+  const addTodo = () => {
     const newTodo = {
-      id:Math.random(),
-      title:input,
-    }
-    setTodos([...todos,newTodo])
-    setInput('')
-  }
+      id: Math.random(),
+      title: input,
+    };
+    setTodos([...todos, newTodo]);
+    setInput("");
+  };
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((input) => input.id !== id));
+  };
+  const editTodo = (id, title) => {
+    setEdit(id);
+    setValue(title);
+  };
+  const saveTodo = (id) => {
+    const newTodo = [...todos].map((input) => {
+      if (input.id === id) {
+        input.title = value;
+      }
+      return input;
+    });
+    setTodos(newTodo);
+    setEdit(null);
+  };
   return (
     <div>
       <input
@@ -22,10 +40,27 @@ const App = () => {
       />
       <button onClick={addTodo}>Add</button>
       <ul>
-      {todos.map(input =>(
-        <li></li>
+        {todos.map((input) => (
+          <li key ={input.id}>
+          {edit===input.id?
+        <div>
+          <input type="text"
+          onChange={(e)=>setValue(e.target.value)}
+          value={value} />
+        </div>: 
+        <div>{input.title}</div> 
+        }
+        {input?
+      <div>
+        <button onClick={()=>deleteTodo(input.id)}>Delete</button>
+        <button onClick={()=>editTodo(input.id,input.title)}>Edit</button>
+        <button onClick={()=>saveTodo(input.id)}>Save</button>
+      </div>:
+      <div></div>  
+      }
+          </li>
         ))}
-        </ul>
+      </ul>
     </div>
   );
 };
